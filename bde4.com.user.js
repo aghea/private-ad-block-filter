372a93236369fdca8844c6cf1c9e7bbff89902f9
ageha <rayyu8@gmail.com>
2021-02-10 23:29:36 +0800

    update common.js

--- bde4.com.user.js
+++ bde4.com.user.js
@@ -7,13 +7,14 @@
 // @namespace         ageha.com/bde4
 // @author            ageha
 // @license           BSD 3-clause Clear License
-// @version           1.1.1
+// @version           1.1.2
 // @grant             none
 // @include           *://bde4.com/*
 // @include           *://bde4.cc/*
 // @updateURL         https://gitee.com/ageha/ad-block-filter/raw/master/bde4.com.user.js
 // @downloadURL       https://gitee.com/ageha/ad-block-filter/raw/master/bde4.com.user.js
 // @installURL        https://gitee.com/ageha/ad-block-filter/raw/master/bde4.com.user.js
+// @require           https://gitee.com/ageha/ad-block-filter/raw/master/common/commonFun.js
 // ==/UserScript==
 
 (function () {
@@ -32,14 +33,4 @@
     }
   }
   
-  function getRealStyle(element,styleName){
-    var realStyle = null;
-    if(element.currentStyle){
-      	realStyle = element.currentStyle[styleName];//IE
-      }else if(window.getComputedStyle){
-      	realStyle=window.getComputedStyle(element,null)[styleName];//W3C
-      }
-    return realStyle;
-  }
-
 })();
