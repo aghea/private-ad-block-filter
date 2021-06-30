998c0e25f4049a9ca17f83fa748d47bd6a492329
ageha <rayyu8@gmail.com>
2021-06-30 11:37:26 +0000

    new dl section

--- bde4.cc.user.js
+++ bde4.cc.user.js
@@ -7,7 +7,7 @@
 // @namespace         ageha.com/bde4
 // @author            ageha
 // @license           BSD 3-clause Clear License
-// @version           1.1.6
+// @version           1.1.6.1
 // @grant             none
 // @include           *://bde4.com/*
 // @include           *://bde4.cc/*
@@ -35,6 +35,21 @@
       || div.className == "download-help"){
       //alert(realPosition);
       div.parentNode.removeChild(div);
+    }
+    
+    var dls = document.getElementsByTagName("dl");
+    for (var dl of dls){
+        if (dl.className == "dl-inline"){
+            dl.parentNode.removeChild(dl);
+        }
+    }
+
+    var sections = document.getElementsByTagName("section");
+    for (var sec of sections){
+        if (sec.className == "abstract mobile-hide"){
+            sec.parentNode.removeChild(sec.nextSibling.nextSibling);
+            sec.parentNode.removeChild(sec);
+        }
     }
 
   }
