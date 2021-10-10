847dec044627ae66377e2e5e4d0b20ed44f910b7
ageha <rayyu8@gmail.com>
2021-10-10 11:13:33 +0000

    add emuleOpen.user.js.

---
+++ emuleOpen.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @homepageURL       https://gitee.com/ageha/ad-block-filter
+// @homepage          https://gitee.com/ageha/ad-block-filter
+// @website           https://gitee.com/ageha/ad-block-filter
+// @include     http://agiha.vicp.net:4711/*
+// @include     http://agiha.vicp.net:4721/*
+// @version     1
+// @grant       none
+// @run-at      document-end
+// ==/UserScript==
+(function(){
+    ShowHide('ed2k_open');
+})();
