245fabde01ba705493fbe9683a87556632ae0bce
ageha <rayyu8@gmail.com>
2021-10-10 17:13:47 +0000

    update

--- emuleOpen.user.js
+++ emuleOpen.user.js
@@ -1,10 +1,18 @@
 // ==UserScript==
+// @name              emule_open_ed2k_textarea
+// @description       自动打开emule输入框
+// @namespace         ageha.com/emule
+// @author            ageha
+// @license           BSD 3-clause Clear License
+// @version           0.1
 // @homepageURL       https://gitee.com/ageha/ad-block-filter
 // @homepage          https://gitee.com/ageha/ad-block-filter
 // @website           https://gitee.com/ageha/ad-block-filter
 // @include     http://agiha.vicp.net:4711/*
 // @include     http://agiha.vicp.net:4721/*
-// @version     1
+// @updateURL         https://gitee.com/ageha/ad-block-filter/raw/master/emuleOpen.user.js
+// @downloadURL       https://gitee.com/ageha/ad-block-filter/raw/master/emuleOpen.user.js
+// @installURL        https://gitee.com/ageha/ad-block-filter/raw/master/emuleOpen.user.js
 // @grant       none
 // @run-at      document-end
 // ==/UserScript==
