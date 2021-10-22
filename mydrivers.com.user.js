11df75bb4040be471d3ac2036f55691e5a89fdca
ageha <rayyu8@gmail.com>
2021-10-22 11:41:08 +0000

    regex

--- mydrivers.com.user.js
+++ mydrivers.com.user.js
@@ -15,8 +15,8 @@
 // @installURL        https://gitee.com/ageha/ad-block-filter/raw/master/mydrivers.com.user.js
 // ==/UserScript==
 function removeLuoyonghao(){
-    //到手 低至 限时
-    var chuxiao = /\u5230\u624b|\u4f4e\u81f3|\u9650\u65f6/;
+    //到手 低至 限时 新低
+    var chuxiao = /\u5230\u624b|\u4f4e\u81f3|\u9650\u65f6|\u65b0\u4f4e/;
     //数字元
     var yuan = /\d{1,4}(\.[0-9]{1,2})?\u5143/;
     //京东|网易|百度|腾讯|芒果|优酷|爱奇艺
@@ -33,7 +33,7 @@ function removeLuoyonghao(){
     //"罗永浩","李国庆","立减","到手","低至","大促","预售","包邮","直降"
     var re3=/\u7f57\u6c38\u6d69|\u674e\u56fd\u5e86|\u7acb\u51cf|\u5230\u624b|\u4f4e\u81f3|\u5927\u4fc3|\u9884\u552e|\u5305\u90ae|\u76f4\u964d/
     var reArray=[re1
-                 ,re2
+                 //,re2
                  ,re3
                 ];
     var spans = document.getElementsByClassName("titl");
