337d328d41cbb1dfbc71b07094aa2509af03f0d4
aghea <106022731+aghea@users.noreply.github.com>
2022-05-22 22:50:51 +0800

    Update emuleOpen.user.js
    update url & f**k gitee.com

--- emuleOpen.user.js
+++ emuleOpen.user.js
@@ -4,15 +4,15 @@
 // @namespace         ageha.com/emule
 // @author            ageha
 // @license           BSD 3-clause Clear License
-// @version           0.1
-// @homepageURL       https://gitee.com/ageha/ad-block-filter
-// @homepage          https://gitee.com/ageha/ad-block-filter
-// @website           https://gitee.com/ageha/ad-block-filter
+// @version           1.0
+// @homepageURL       https://github.com/aghea/private-ad-block-filter
+// @homepage          https://github.com/aghea/private-ad-block-filter
+// @website           https://github.com/aghea/private-ad-block-filter
 // @include     http://agiha.vicp.net:4711/*
 // @include     http://agiha.vicp.net:4721/*
-// @updateURL         https://gitee.com/ageha/ad-block-filter/raw/master/emuleOpen.user.js
-// @downloadURL       https://gitee.com/ageha/ad-block-filter/raw/master/emuleOpen.user.js
-// @installURL        https://gitee.com/ageha/ad-block-filter/raw/master/emuleOpen.user.js
+// @updateURL         https://github.com/aghea/private-ad-block-filter/raw/master/emuleOpen.user.js
+// @downloadURL       https://github.com/aghea/private-ad-block-filter/raw/master/emuleOpen.user.js
+// @installURL        https://github.com/aghea/private-ad-block-filter/raw/master/emuleOpen.user.js
 // @grant       none
 // @run-at      document-end
 // ==/UserScript==
