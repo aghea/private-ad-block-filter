7ff9329b213e62b61ad11678e094c14a4bcf0290
aghea <106022731+aghea@users.noreply.github.com>
2022-06-15 15:11:04 +0800

    Update showAllPage.user.js
    update url & f**k gitee

--- showAllPage.user.js
+++ showAllPage.user.js
@@ -1,18 +1,18 @@
 // ==UserScript==
 // @name         自动打开全文
-// @homepageURL       https://gitee.com/ageha/ad-block-filter
-// @homepage          https://gitee.com/ageha/ad-block-filter
-// @website           https://gitee.com/ageha/ad-block-filter
-// @version      0.1
+// @homepageURL       https://github.com/aghea/private-ad-block-filter
+// @homepage          https://github.com/aghea/private-ad-block-filter
+// @website           https://github.com/aghea/private-ad-block-filter
+// @version      0.2
 // @description  自动打开全文
 // @author       ageha
 // @license           BSD 3-clause Clear License
 // @match        https://*.zol.com.cn/*
 // @match        https://news.mydrivers.com/*
 // @grant        none
-// @updateURL         https://gitee.com/ageha/ad-block-filter/raw/master/showAllPage.user.js
-// @downloadURL       https://gitee.com/ageha/ad-block-filter/raw/master/showAllPage.user.js
-// @installURL        https://gitee.com/ageha/ad-block-filter/raw/master/showAllPage.user.js
+// @updateURL         https://github.com/aghea/private-ad-block-filter/raw/master/showAllPage.user.js
+// @downloadURL       https://github.com/aghea/private-ad-block-filter/raw/master/showAllPage.user.js
+// @installURL        https://github.com/aghea/private-ad-block-filter/raw/master/showAllPage.user.js
 
 // ==/UserScript==
 function showAll(){
