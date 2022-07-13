284f48a38b83d601b033f9a211c83eb625cb070b
aghea <106022731+aghea@users.noreply.github.com>
2022-07-13 19:32:07 +0800

    Update removeMydriversPic.user.js

--- removeMydriversPic.user.js
+++ removeMydriversPic.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         移除mydrivers最后一张无用图片
+// @name         移除mydrivers最后一张无用图片和评论导向
 // @homepageURL       https://github.com/aghea/private-ad-block-filter
 // @homepage          https://github.com/aghea/private-ad-block-filter
 // @website           https://github.com/aghea/private-ad-block-filter
-// @version      1.0
-// @description  移除mydrivers最后一张无用图片
+// @version      2.0
+// @description  移除mydrivers最后一张无用图片和评论导向
 // @author       ageha
 // @license           BSD 3-clause Clear License
 // @match        https://news.mydrivers.com/*
@@ -26,6 +26,20 @@ function removeImg(){
         div.removeChild(obj);
     }
 }
+function removeOp(){
+    //对此|你怎么看
+    var reg = /\u5bf9\u6b64|\u4f60\u600e\u4e48\u770b/;
+    var divs = document.getElementsByClassName("news_info");
+    var div = divs[0];
+    var plinks = div.getElementsByTagName("p");
+    for(var idx = plinks.length - 1; idx > -1 ; idx --){
+        if(reg.test(plinks[idx].innerHTML)){
+            div.removeChild(plinks[idx]);
+            break;
+        }
+    }
+}
 (function() {
     removeImg();
+    removeOp();
 })();
