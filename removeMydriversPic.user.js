045030d3cf3e7f0769cf2f26e86d692fa33eaf15
aghea <106022731+aghea@users.noreply.github.com>
2022-07-19 13:40:57 +0800

    Update removeMydriversPic.user.js

--- removeMydriversPic.user.js
+++ removeMydriversPic.user.js
@@ -3,7 +3,7 @@
 // @homepageURL       https://github.com/aghea/private-ad-block-filter
 // @homepage          https://github.com/aghea/private-ad-block-filter
 // @website           https://github.com/aghea/private-ad-block-filter
-// @version      2.0
+// @version      2.1
 // @description  移除mydrivers最后一张无用图片和评论导向
 // @author       ageha
 // @license           BSD 3-clause Clear License
@@ -27,15 +27,22 @@ function removeImg(){
     }
 }
 function removeOp(){
-    //对此|你怎么看
-    var reg = /\u5bf9\u6b64|\u4f60\u600e\u4e48\u770b/;
+    var regArray = [
+        //对此|你怎么看
+        /\u5bf9\u6b64/,/\u4f60\u600e\u4e48\u770b/
+        ,
+        //你喜欢|吗？
+        /\u4f60\u559c\u6b22/,/\u5417\uff1f/
+    ];
     var divs = document.getElementsByClassName("news_info");
     var div = divs[0];
     var plinks = div.getElementsByTagName("p");
     for(var idx = plinks.length - 1; idx > -1 ; idx --){
-        if(reg.test(plinks[idx].innerHTML)){
-            div.removeChild(plinks[idx]);
-            break;
+        for(var jdx = 0; jdx <regArray.length ; jdx+=2){
+            if(regArray[jdx].test(plinks[idx].innerHTML)&&regArray[jdx+1].test(plinks[idx].innerHTML)){
+                div.removeChild(plinks[idx]);
+                break;
+            }
         }
     }
 }
