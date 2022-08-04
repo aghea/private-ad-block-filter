c2eee89a686fd424788537c667617d62930ce593
aghea <106022731+aghea@users.noreply.github.com>
2022-08-04 11:57:56 +0800

    Update removeMydriversPic.user.js

--- removeMydriversPic.user.js
+++ removeMydriversPic.user.js
@@ -3,7 +3,7 @@
 // @homepageURL       https://github.com/aghea/private-ad-block-filter
 // @homepage          https://github.com/aghea/private-ad-block-filter
 // @website           https://github.com/aghea/private-ad-block-filter
-// @version      4.2.0.0
+// @version      4.3.0.0
 // @description  移除mydrivers最后一张无用图片和评论导向
 // @author       ageha
 // @license           BSD 3-clause Clear License
@@ -75,8 +75,7 @@ function removeOp1(){
         /\u5417\uff1f$/
         ,/\u5417\u003f$/
         //怎么看|样？
-        ,/\u600e\u4e48[\u770b|\u6837]\uff1f$/
-        ,/\u600e\u4e48[\u770b|\u6837]\u003f$/
+        ,/\u600e\u4e48[\u770b|\u6837][\uff1f|\u003f]/
         /*
         //怎么看？
         ,/\u600e\u4e48\u770b\uff1f$/
