8dc0fe44f4035a754beecb93a242b2532db3fffa
aghea <106022731+aghea@users.noreply.github.com>
2022-08-18 01:33:11 +0800

    Update removeMydriversComments.user.js

--- removeMydriversComments.user.js
+++ removeMydriversComments.user.js
@@ -3,7 +3,7 @@
 // @homepageURL       https://github.com/aghea/private-ad-block-filter
 // @homepage          https://github.com/aghea/private-ad-block-filter
 // @website           https://github.com/aghea/private-ad-block-filter
-// @version      1.0.0.0
+// @version      1.1.0.0
 // @description  移除mydrivers页面评论导向
 // @author       ageha
 // @license           BSD 3-clause Clear License
@@ -117,6 +117,7 @@ fuction removeDivWidth(){
 }
 (function() {
     removeMinHigh();
+    removeDivWidth();
     if(removeOp1()){
         return;
     }
