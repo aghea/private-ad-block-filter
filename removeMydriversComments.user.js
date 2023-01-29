c4cf6730335d11fcd7569124c9a3090f27be3e1c
aghea <106022731+aghea@users.noreply.github.com>
2023-01-29 20:03:40 +0800

    Update removeMydriversComments.user.js

--- removeMydriversComments.user.js
+++ removeMydriversComments.user.js
@@ -3,7 +3,7 @@
 // @homepageURL       https://github.com/aghea/private-ad-block-filter
 // @homepage          https://github.com/aghea/private-ad-block-filter
 // @website           https://github.com/aghea/private-ad-block-filter
-// @version      1.2.1.6
+// @version      1.2.1.7
 // @description  移除mydrivers页面评论导向
 // @author       ageha
 // @license           BSD 3-clause Clear License
@@ -21,7 +21,7 @@ var div = divs[0];
 //移除页面内strong文字
 function replaceStrong(){
             var text = div.innerHTML.toString();
-            var t = text.replaceAll("<strong>"," ").replaceAll("</strong>"," ").replaceAll("color:"," ");
+            var t = text.replaceAll("<strong>"," ").replaceAll("<strong ","< ").replaceAll("</strong>"," ").replaceAll("color:"," ");
             div.innerHTML=t;
 }
 //获得最后一个p标签
