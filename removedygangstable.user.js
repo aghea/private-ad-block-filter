d703adc503830186c039ae4573a943dd511cf720
aghea <106022731+aghea@users.noreply.github.com>
2024-06-18 19:07:20 +0800

    Update removedygangstable.user.js

--- removedygangstable.user.js
+++ removedygangstable.user.js
@@ -8,7 +8,7 @@
 // @description  电影港简介无用内容删除
 // @author            ageha
 // @license           BSD 3-clause Clear License
-// @match        https://www.dygangs.net/ys/*
+// @match        https://www.dygangs.net/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=dygangs.net
 // @updateURL         https://github.com/aghea/private-ad-block-filter/raw/master/removedygangstable.user.js
 // @downloadURL       https://github.com/aghea/private-ad-block-filter/raw/master/removedygangstable.user.js
