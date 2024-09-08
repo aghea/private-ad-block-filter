e2846c5920385e4139b643ad4a76ae11683cfcee
aghea <106022731+aghea@users.noreply.github.com>
2024-09-08 11:50:19 +0800

    Create DelMp4.comTopDiv.user.js

---
+++ DelMp4.comTopDiv.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Del mp4 top div
+// @homepageURL       https://github.com/aghea/private-ad-block-filter
+// @homepage          https://github.com/aghea/private-ad-block-filter
+// @website           https://github.com/aghea/private-ad-block-filter
+// @namespace    ageha.com/domp4
+// @version      2024-09-07
+// @description  删除mp4的top div
+// @author       ageha
+// @match        https://www.mp4us.com/html/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=mp4us.com
+// @license           BSD 3-clause Clear License
+// @updateURL         https://github.com/aghea/private-ad-block-filter/raw/master/DelMp4.comTopDiv.user.js
+// @downloadURL       https://github.com/aghea/private-ad-block-filter/raw/master/DelMp4.comTopDiv.user.js
+// @installURL        https://github.com/aghea/private-ad-block-filter/raw/master/DelMp4.comTopDiv.user.js
+// @require      https://lf3-cdn-tos.bytecdntp.com/cdn/expire-1-M/jquery-cookie/1.4.1/jquery.cookie.min.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+/* globals jQuery, $, waitForKeyElements */
+    $('#top').remove();
+})();
