b5777c94fe6f1c27504d3afe7f79fac9cb2c6488
aghea <106022731+aghea@users.noreply.github.com>
2024-12-10 16:16:18 +0800

    Update OpenBdysSynopsis.user.js

--- OpenBdysSynopsis.user.js
+++ OpenBdysSynopsis.user.js
@@ -1,19 +1,19 @@
 // ==UserScript==
-// @name         bdys自动打开剧情介绍
+// @name              bdys自动打开剧情介绍
 // @homepageURL       https://github.com/aghea/private-ad-block-filter/
 // @homepage          https://github.com/aghea/private-ad-block-filter/
 // @website           https://github.com/aghea/private-ad-block-filter/
-// @version      2.0
-// @description  bdys自动打开剧情介绍
+// @version           2.0
+// @description       bdys自动打开剧情介绍
 // @namespace         ageha.com/bde4
 // @author            ageha
 // @license           BSD 3-clause Clear License
-// @match        *://*.*.*/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=bdys01.com
+// @include           *://*.*.*/*
+// @icon              https://www.google.com/s2/favicons?sz=64&domain=bdys01.com
 // @updateURL         https://github.com/aghea/private-ad-block-filter/raw/master/OpenBdysSynopsis.user.js
 // @downloadURL       https://github.com/aghea/private-ad-block-filter/raw/master/OpenBdysSynopsis.user.js
 // @installURL        https://github.com/aghea/private-ad-block-filter/raw/master/OpenBdysSynopsis.user.js
-// @grant        none
+// @grant             none
 // ==/UserScript==
 
 (function() {
