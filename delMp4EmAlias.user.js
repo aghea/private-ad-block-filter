db81eba0f87a7a2b82933f1eccaa0b92dfd195fd
aghea <106022731+aghea@users.noreply.github.com>
2025-02-19 13:09:08 +0800

    Update delMp4EmAlias.user.js

--- delMp4EmAlias.user.js
+++ delMp4EmAlias.user.js
@@ -4,10 +4,11 @@
 // @homepage          https://github.com/aghea/private-ad-block-filter
 // @website           https://github.com/aghea/private-ad-block-filter
 // @namespace    ageha.com/domp4
-// @version      0.0.0.3
+// @version      0.0.0.4
 // @author       ageha
-// @match        https://www.mp4us.com/*.*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=mp4us.com
+// @include        https://www.mp4us.com/*.*
+// @include        https://www.xlmp4.com/*.*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=xlmp4.com
 // @license           BSD 3-clause Clear License
 // @updateURL         https://github.com/aghea/private-ad-block-filter/raw/master/delMp4EmAlias.user.js
 // @downloadURL       https://github.com/aghea/private-ad-block-filter/raw/master/delMp4EmAlias.user.js
