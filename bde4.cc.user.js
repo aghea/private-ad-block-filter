0f79017779f263905d6672c4583a579b3242adf7
aghea <106022731+aghea@users.noreply.github.com>
2025-02-24 21:17:07 +0800

    Update bde4.cc.user.js

--- bde4.cc.user.js
+++ bde4.cc.user.js
@@ -7,13 +7,14 @@
 // @namespace         ageha.com/bde4
 // @author            ageha
 // @license           BSD 3-clause Clear License
-// @version           2.3.0.0
+// @version           2.3.0.1
 // @grant             none
 // @include           *://*.bde4.*/*
 // @include           *://*.mp4er.*/*
 // @include           *://*.*bdys*.*/*
 // @include           *://*.yjys*.*/*
 // @include           *://*.xlys*.*/*
+// @include           *://v.xlys.ltd.ua
 // @updateURL         https://github.com/aghea/private-ad-block-filter/raw/master/bde4.cc.user.js
 // @downloadURL       https://github.com/aghea/private-ad-block-filter/raw/master/bde4.cc.user.js
 // @installURL        https://github.com/aghea/private-ad-block-filter/raw/master/bde4.cc.user.js
