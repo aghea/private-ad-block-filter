464f382d3412f091ab618a45f49c693803078743
aghea <106022731+aghea@users.noreply.github.com>
2025-04-11 19:35:31 +0800

    Update delMp4EmAlias.user.js

--- delMp4EmAlias.user.js
+++ delMp4EmAlias.user.js
@@ -4,10 +4,9 @@
 // @homepage          https://github.com/aghea/private-ad-block-filter
 // @website           https://github.com/aghea/private-ad-block-filter
 // @namespace    ageha.com/domp4
-// @version      0.0.0.4
+// @version      1.0.0.0
 // @author       ageha
-// @include        https://www.mp4us.com/*.*
-// @include        https://www.xlmp4.com/*.*
+// @include        https://*.*.*/*.*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=xlmp4.com
 // @license           BSD 3-clause Clear License
 // @updateURL         https://github.com/aghea/private-ad-block-filter/raw/master/delMp4EmAlias.user.js
@@ -17,13 +16,16 @@
 // ==/UserScript==
 (function() {
     'use strict';
-    var ems = document.getElementsByTagName("em");
-    try{
-        for(var idx = 0; idx < ems.length; idx++){
-            if(ems[idx].innerText == "别名："){
-                ems[idx].innerText = " ";
-                break;
+     const theHead = document.title;
+    if(theHead.indexOf("Mp4电影") > -1){
+        var ems = document.getElementsByTagName("em");
+        try{
+            for(var idx = 0; idx < ems.length; idx++){
+                if(ems[idx].innerText == "别名："){
+                    ems[idx].innerText = " ";
+                    break;
+                }
             }
-        }
-    }catch(err){}
+        }catch(err){}
+    }
 })();
